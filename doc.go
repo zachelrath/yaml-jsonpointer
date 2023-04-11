d250c5e27dcfc8f37f4c1b83b72e2d8c603ae95f
Zach McElrath <zach@ues.io>
2023-04-11 09:11:43 -0400

    docs: Switch to MIT license

--- doc.go
+++ doc.go
@@ -1,5 +1,4 @@
-// Copyright 2020 VMware, Inc.
-// SPDX-License-Identifier: BSD-2-Clause
+// Copyright 2023 Zach McElrath
 
 /*
 Package yptr is a JSONPointer implementation that can walk though a yaml.Node tree.
@@ -13,13 +12,13 @@ which usually contain arrays whose elements are objects with a field that unique
 
 For example, given a JSON/YAML input document:
 
-    {"a": [{"k":"x", "v": 42}, {"k":"y", "v": 77}]}
+	{"a": [{"k":"x", "v": 42}, {"k":"y", "v": 77}]}
 
 If "k" is a field that contains a key that uniquiely identifies an element in a given array,
 we can select the node with the scalar 42 by first selecting the array element for which "k"
 has the value of "x", and then by walking to the field "v":
 
-    /a/~{"k":"x"}/v
+	/a/~{"k":"x"}/v
 
 The "~" token accepts an argument which is interpreted as JSON value to be used as "query-by-example" filter
 against elements of an array.
@@ -31,6 +30,5 @@ This library offers an API to retrieve multiple matches ("FindAll") or to fetch 
 JSONPointer is designed to locate exactly one node in the tree. This can be achieved only if the effective
 schema of the JSON/YAML document mandates that there is an identifying key in each array element you want to point to.
 Using the "Find" function effectively performs a dynamic check of that invariant.
-
 */
 package yptr
